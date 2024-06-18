093de91574b3ae6facc41a0c7c25b868a211fd08
Dpaola-G <Dpaolag17@gmail.com>
2024-06-18 13:35:11 -0500

    99.9%

--- javascript/ejercicio1.js
+++
@@ -1,8 +0,0 @@
-// Escribe un programa que pida al usuario un número y muestre si es par o impar
-let numero= prompt('Ingresa un número');
-
-if(numero / 2 === 0) {
-    alert('Es un número par.');
-} else{
-    alert('Es un número impar.');
-}
